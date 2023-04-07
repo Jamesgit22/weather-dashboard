176d420b9d68163ddf232090de4075bc36131223
Jamesgit22 <jschroeder97@icloud.com>
2023-04-07 16:21:10 -0500

    Added data from forcast api to an array to later convert into readable days. Also turned the html creation into a function to call.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
   let hQuery;
   let histBtnLat;
   let histBtnLon;
+  let jsonDataDateConv = [];
 
   //   API URLs
 
@@ -24,120 +25,7 @@ $(document).ready(function () {
     localStorage.setItem("storedArray", JSON.stringify(localStorageArray));
 
     // Dynamically create html elements to store data in info section
-    $("#info-section").html(`<div class="col-12 my-3">
-        <div class="col-12">
-            <div class="col-12 rounded border border-dark p-1">
-                <div class="row">
-                    <div class="col-2 fs-3 fw-bold">Orlando</div>
-                    <div class="col-10 fs-3 fw-bold">(04/05/2023)</div>
-                </div>
-                <div class="row">
-                    <div class="col-12 py-3">Temp: <span id="degrees"></span> ^F</div>
-                </div>
-                <div class="row">
-                    <div class="col-12 py-3">Wind: <span id="wind-speed"></span> MPH</div>
-                </div>
-                <div class="row">
-                    <div class="col-12 py-3">Humidity: <span id="humidity"></span>%</div>
-                </div>
-            </div>
-        </div>
-    <!-- 5 day forecast section -->
-        <div class="row">
-            <div class="col-12 py-2 fw-bold">5-Day Forecast:</div>
-        </div>
-        <div class="row">
-            <div class="col-12 ps-4">
-                <div class="row justify-content-between">
-                    <div class="col-2 p-1 bg-cust-forecast text-light">
-                        <div class="row">
-                            <div class="col-12 fw-bold">4/6/2023</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1"><span id="icon-f-1"></span></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Temp: <span id="degrees-f-1"></span> ^F</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Wind: <span id="wind-speed-f-1"></span> MPH</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Humidity: <span id="humidity-f-1"></span>%</div>
-                        </div>
-                    </div>
-                    <div class="col-2 p-1 bg-cust-forecast text-light">
-                        <div class="row">
-                            <div class="col-12 fw-bold">4/6/2023</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1"><span id="icon-f-2"></span></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Temp: <span id="degrees-f-2"></span> ^F</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Wind: <span id="wind-speed-f-2"></span> MPH</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Humidity: <span id="humidity-f-2"></span>%</div>
-                        </div>
-                    </div>
-                    <div class="col-2 p-1 bg-cust-forecast text-light">
-                        <div class="row">
-                            <div class="col-12 fw-bold">4/6/2023</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1"><span id="icon-f-3"></span></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Temp: <span id="degrees-f-3"></span> ^F</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Wind: <span id="wind-speed-f-3"></span> MPH</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Humidity: <span id="humidity-f-3"></span>%</div>
-                        </div>
-                    </div>
-                    <div class="col-2 p-1 bg-cust-forecast text-light">
-                        <div class="row">
-                            <div class="col-12 fw-bold">4/6/2023</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1"><span id="icon-f-4"></span></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Temp: <span id="degrees-f-4"></span> ^F</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Wind: <span id="wind-speed-f-4"></span> MPH</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Humidity: <span id="humidity-f-4"></span>%</div>
-                        </div>
-                    </div>
-                    <div class="col-2 p-1 bg-cust-forecast text-light">
-                        <div class="row">
-                            <div class="col-12 fw-bold">4/6/2023</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1"><span id="icon-f-5"></span></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Temp: <span id="degrees-f-5"></span> ^F</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Wind: <span id="wind-speed-f-5"></span> MPH</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Humidity: <span id="humidity-f-5"></span>%</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>`);
+    createHistHTML() 
   });
 
   // Create new buttons with each search click
@@ -176,120 +64,6 @@ $(document).ready(function () {
   $("#history-container").on("click", ".history-click", function (e) {
     e.stopPropagation();
     // Create HTML content
-    $("#info-section").html(`<div class="col-12 my-3">
-        <div class="col-12">
-            <div class="col-12 rounded border border-dark p-1">
-                <div class="row">
-                    <div class="col-2 fs-3 fw-bold">Orlando</div>
-                    <div class="col-10 fs-3 fw-bold">(04/05/2023)</div>
-                </div>
-                <div class="row">
-                    <div class="col-12 py-3">Temp: <span id="degrees"></span> ^F</div>
-                </div>
-                <div class="row">
-                    <div class="col-12 py-3">Wind: <span id="wind-speed"></span> MPH</div>
-                </div>
-                <div class="row">
-                    <div class="col-12 py-3">Humidity: <span id="humidity"></span>%</div>
-                </div>
-            </div>
-        </div>
-    <!-- 5 day forecast section -->
-        <div class="row">
-            <div class="col-12 py-2 fw-bold">5-Day Forecast:</div>
-        </div>
-        <div class="row">
-            <div class="col-12 ps-4">
-                <div class="row justify-content-between">
-                    <div class="col-2 p-1 bg-cust-forecast text-light">
-                        <div class="row">
-                            <div class="col-12 fw-bold">4/6/2023</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1"><span id="icon-f-1"></span></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Temp: <span id="degrees-f-1"></span> ^F</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Wind: <span id="wind-speed-f-1"></span> MPH</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Humidity: <span id="humidity-f-1"></span>%</div>
-                        </div>
-                    </div>
-                    <div class="col-2 p-1 bg-cust-forecast text-light">
-                        <div class="row">
-                            <div class="col-12 fw-bold">4/6/2023</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1"><span id="icon-f-2"></span></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Temp: <span id="degrees-f-2"></span> ^F</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Wind: <span id="wind-speed-f-2"></span> MPH</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Humidity: <span id="humidity-f-2"></span>%</div>
-                        </div>
-                    </div>
-                    <div class="col-2 p-1 bg-cust-forecast text-light">
-                        <div class="row">
-                            <div class="col-12 fw-bold">4/6/2023</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1"><span id="icon-f-3"></span></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Temp: <span id="degrees-f-3"></span> ^F</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Wind: <span id="wind-speed-f-3"></span> MPH</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Humidity: <span id="humidity-f-3"></span>%</div>
-                        </div>
-                    </div>
-                    <div class="col-2 p-1 bg-cust-forecast text-light">
-                        <div class="row">
-                            <div class="col-12 fw-bold">4/6/2023</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1"><span id="icon-f-4"></span></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Temp: <span id="degrees-f-4"></span> ^F</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Wind: <span id="wind-speed-f-4"></span> MPH</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Humidity: <span id="humidity-f-4"></span>%</div>
-                        </div>
-                    </div>
-                    <div class="col-2 p-1 bg-cust-forecast text-light">
-                        <div class="row">
-                            <div class="col-12 fw-bold">4/6/2023</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1"><span id="icon-f-5"></span></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Temp: <span id="degrees-f-5"></span> ^F</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Wind: <span id="wind-speed-f-5"></span> MPH</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 py-1">Humidity: <span id="humidity-f-5"></span>%</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>`);
   });
 
   // function to get latitude and longitude of city search
@@ -297,6 +71,7 @@ $(document).ready(function () {
     e.stopPropagation();
     hQuery = e.target.textContent;
     logHistGeoJSONData();
+    createHistHTML();
   });
 
   // function to fetch geo API
@@ -305,18 +80,147 @@ $(document).ready(function () {
       `http://api.openweathermap.org/geo/1.0/direct?q=${hQuery}&appid=c664a502c1ab3dc877ac211db4a9428f`
     );
     const jsonData = await response.json();
-    console.log(jsonData[0].lat);
-    console.log(jsonData[0].lon);
+    // console.log(jsonData[0].lat);
+    // console.log(jsonData[0].lon);
     histBtnLat = jsonData[0].lat;
     histBtnLon = jsonData[0].lon;
     logHistForeJSONData();
   }
 
+//   Funciton to fetch 
   async function logHistForeJSONData() {
     const response = await fetch(
       `http://api.openweathermap.org/data/2.5/forecast?lat=${histBtnLat}&lon=${histBtnLon}&appid=c664a502c1ab3dc877ac211db4a9428f`
     );
     const jsonData = await response.json();
-    console.log(jsonData);
+    // console.log(jsonData);
+    // console.log(jsonData.list[0].dt);
+    // console.log(jsonData.list[1].dt);
+    jsonDataDateConv.push(jsonData.list[0].dt);
+    jsonDataDateConv.push(jsonData.list[1].dt);
+    jsonDataDateConv.push(jsonData.list[2].dt);
+    jsonDataDateConv.push(jsonData.list[3].dt);
+    jsonDataDateConv.push(jsonData.list[4].dt);
+    jsonDataDateConv.push(jsonData.list[5].dt);
+    return jsonData;
+  }
+
+  console.log(jsonDataDateConv);
+
+  function createHistHTML() {
+    $("#info-section").html(`<div class="col-12 my-3">
+    <div class="col-12">
+        <div class="col-12 rounded border border-dark p-1">
+            <div class="row">
+                <div class="col-2 fs-3 fw-bold">Orlando</div>
+                <div class="col-10 fs-3 fw-bold">(04/05/2023)</div>
+            </div>
+            <div class="row">
+                <div class="col-12 py-3">Temp: <span id="degrees"></span> ^F</div>
+            </div>
+            <div class="row">
+                <div class="col-12 py-3">Wind: <span id="wind-speed"></span> MPH</div>
+            </div>
+            <div class="row">
+                <div class="col-12 py-3">Humidity: <span id="humidity"></span>%</div>
+            </div>
+        </div>
+    </div>
+<!-- 5 day forecast section -->
+    <div class="row">
+        <div class="col-12 py-2 fw-bold">5-Day Forecast:</div>
+    </div>
+    <div class="row">
+        <div class="col-12 ps-4">
+            <div class="row justify-content-between">
+                <div class="col-2 p-1 bg-cust-forecast text-light">
+                    <div class="row">
+                        <div class="col-12 fw-bold">4/6/2023</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1"><span id="icon-f-1"></span></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Temp: <span id="degrees-f-1"></span> ^F</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Wind: <span id="wind-speed-f-1"></span> MPH</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Humidity: <span id="humidity-f-1"></span>%</div>
+                    </div>
+                </div>
+                <div class="col-2 p-1 bg-cust-forecast text-light">
+                    <div class="row">
+                        <div class="col-12 fw-bold">4/6/2023</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1"><span id="icon-f-2"></span></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Temp: <span id="degrees-f-2"></span> ^F</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Wind: <span id="wind-speed-f-2"></span> MPH</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Humidity: <span id="humidity-f-2"></span>%</div>
+                    </div>
+                </div>
+                <div class="col-2 p-1 bg-cust-forecast text-light">
+                    <div class="row">
+                        <div class="col-12 fw-bold">4/6/2023</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1"><span id="icon-f-3"></span></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Temp: <span id="degrees-f-3"></span> ^F</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Wind: <span id="wind-speed-f-3"></span> MPH</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Humidity: <span id="humidity-f-3"></span>%</div>
+                    </div>
+                </div>
+                <div class="col-2 p-1 bg-cust-forecast text-light">
+                    <div class="row">
+                        <div class="col-12 fw-bold">4/6/2023</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1"><span id="icon-f-4"></span></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Temp: <span id="degrees-f-4"></span> ^F</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Wind: <span id="wind-speed-f-4"></span> MPH</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Humidity: <span id="humidity-f-4"></span>%</div>
+                    </div>
+                </div>
+                <div class="col-2 p-1 bg-cust-forecast text-light">
+                    <div class="row">
+                        <div class="col-12 fw-bold">4/6/2023</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1"><span id="icon-f-5"></span></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Temp: <span id="degrees-f-5"></span> ^F</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Wind: <span id="wind-speed-f-5"></span> MPH</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 py-1">Humidity: <span id="humidity-f-5"></span>%</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`);
   }
 });
